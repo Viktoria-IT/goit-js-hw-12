2946c8e2ae8ebbbd48f79ea1cf792005c6c22459
Viktoria Koval <kovalviktoria.hitech@gmail.com>
2021-07-21 10:48:23 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 import './css/styles.css';
+import countryCardTpl from '../src/templates/country_tmpl.hbs';
+import countriesTpl from '../src/templates/countries.hbs';
+import API from './js/fetchCountries';
+import getRefs from './js/getrefs';
+import Notiflix from "notiflix";
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
-inputSearch: document.getElementById('search-box');
-countryInfo: document.querySelector('.country-info');
+const refs = getRefs();
 
-const BASE_URL = 'https://restcountries.eu/rest/v2';
+refs.inputSearch.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function fetchCountry(countryName) {
-    return fetch(`${BASE_URL}/name/${countryName}`).then(response => 
-            response.json(),
-        );        
+function onSearch() {
+    const name = refs.inputSearch.value;
+    refs.countryInfo.innerHTML = '';
+
+    API.fetchCountry(name)
+        .then(renderCountryCard)
+        .catch(error => console.log(error))             
 }
 
-export default { fetchCountry };
+function renderCountryCard(name) {
+
+    if (name.length === 1) {
+        const markup = name[0];
+        refs.countryInfo.insertAdjacentHTML('beforeend', countryCardTpl(markup));        
+    } else if (name.length > 10) {
+        getInfoMessage('Too many matches found. Please enter a more specific name.');
+    } else if (name.status === 404){
+        getErrorMessage('Oops, there is no country with that name');
+    } else {
+        refs.countryInfo.innerHTML = countriesTpl(name);
+    }
+}
+
+function getInfoMessage(message) {
+    Notiflix.Notify.info(message);
+}
+
+function getErrorMessage(message) {
+    Notiflix.Notify.failure(message);
+}
+ 
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+import '../css/styles.css';
+
+
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountry(countryName) {
+    return fetch(`${BASE_URL}/name/${countryName}`).then(response => 
+            response.json(),
+        );        
+}
+
+export default { fetchCountry };
